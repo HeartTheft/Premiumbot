c2b85d8d2014f53d35f5ffc40fb30322163a1dcf
HeartTheft <110565010+HeartTheft@users.noreply.github.com>
2024-10-03 22:20:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '22977776'))
 API_HASH = environ.get('API_HASH', '2ac7223d720bdeec757cbc88ced57224')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6700618694:AAHebNRWVOrwwS07zDdFoj2drE3Hw8x8Xao')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7041695823:AAG0csbGjGcOdal3gsWzCOZvsoQR1n3A-3k')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6762558871').split()]
